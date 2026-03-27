e479a67e251ba9e8024347e0038ccfe1ba0e6183
Yaman <yamana192@gmail.com>
2026-03-27 05:56:59 +0300

    Add Memory Caching and finalize Weather API

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,5 +1,8 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Caching.Memory;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -21,5 +24,19 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-app.MapGet("/weather/{city}", (string city) => $"Weather data for {city} will be here soon!");
+app.MapGet("/weather/{city}", async (string city, IConfiguration config, IHttpClientFactory httpClientFactory, Microsoft.Extensions.Caching.Memory.IMemoryCache cache) =>
+{
+    string cashKey = $"weather_{city}";
+    if (cache.TryGetValue(cashKey, out string cacheWeather))
+    {
+        return Results.Content(cacheWeather, "application/json");
+    }
+    var apiKey = config["WeatherAPI:ApiKey"];
+    var url = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&key={apiKey}";
+    var client = httpClientFactory.CreateClient();
+    var response = await client.GetAsync(url);
+    var weatherData = await response.Content.ReadAsStringAsync();
+    cache.Set(cashKey, weatherData, TimeSpan.FromHours(12));
+    return Results.Content(weatherData, "application/json");
+});
 app.Run();
